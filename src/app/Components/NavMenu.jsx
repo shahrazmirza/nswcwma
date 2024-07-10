9b69aff24c80c1c8be3240fe9db1bd7c91848ff1
shahrazmirza <shahrazahmedmirza@live.com>
2024-07-10 16:57:34 +1000

    Commit

--- src/app/Components/NavMenu.jsx
+++ src/app/Components/NavMenu.jsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import React, { useState, useEffect, useContext } from "react";
 import { Container } from "@radix-ui/themes";
 import Image from "next/image";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import classNames from "classnames";
 
 const NavMenu = () => {
   const [activeLink, setActiveLink] = useState("");
@@ -50,14 +52,109 @@ const NavMenu = () => {
             </Link>
           </div>
 
+          {/* <NavigationMenu.Root>
+            <NavigationMenu.List className="flex text-white text-sm font-normal gap-1">
+              <NavigationMenu.Item>
+                <NavigationMenu.Trigger
+                  href="/"
+                  className={`uppercase outline outline-1 py-1 px-4 hover:outline-gray-300 ${
+                    activeLink === "/" ? "outline-gray-300" : "outline-gray-800"
+                  }`}
+                  onClick={() => handleLinkClick("/")}
+                >
+                  Home
+                </NavigationMenu.Trigger>
+              </NavigationMenu.Item>
+
+              <NavigationMenu.Item>
+                <NavigationMenu.Trigger
+                  href="/About"
+                  className={`uppercase outline outline-1 py-1 px-4 hover:outline-gray-300 ${
+                    activeLink === "/About"
+                      ? "outline-gray-300"
+                      : "outline-gray-800"
+                  }`}
+                  onClick={() => handleLinkClick("/About")}
+                >
+                  About
+                </NavigationMenu.Trigger>
+              </NavigationMenu.Item>
+
+              <NavigationMenu.Item>
+                <NavigationMenu.Trigger
+                  className={`uppercase outline outline-1 py-1 px-4 hover:outline-gray-300 ${
+                    activeLink === "/Services"
+                      ? "outline-gray-300"
+                      : "outline-gray-800"
+                  }`}
+                  onClick={() => handleLinkClick("/Services")}
+                >
+                  Services
+                </NavigationMenu.Trigger>
+                <NavigationMenu.Content>
+                  <ul className="bg-white bg-opacity-100 text-black mt-0 w-fit text-sm capitalize text-center">
+                    <div className="border-t-5 border-red-500 mt-2"></div>
+                    <ListItem
+                      className="transition delay-150 duration-300 ease-in-out hover:bg-black hover:text-white"
+                      title="Homeless"
+                      href="/Homeless"
+                    ></ListItem>
+                  </ul>
+                </NavigationMenu.Content>
+              </NavigationMenu.Item>
+
+              <NavigationMenu.Item>
+                <NavigationMenu.Trigger
+                  href="/Events"
+                  className={`uppercase outline outline-1 py-1 px-4 hover:outline-gray-300 ${
+                    activeLink === "/Events"
+                      ? "outline-gray-300"
+                      : "outline-gray-800"
+                  }`}
+                  onClick={() => handleLinkClick("/Events")}
+                >
+                  Events
+                </NavigationMenu.Trigger>
+              </NavigationMenu.Item>
+
+              <NavigationMenu.Item>
+                <NavigationMenu.Trigger
+                  href="/Downloads"
+                  className={`uppercase outline outline-1 py-1 px-4 hover:outline-gray-300 ${
+                    activeLink === "/Downloads"
+                      ? "outline-gray-300"
+                      : "outline-gray-800"
+                  }`}
+                  onClick={() => handleLinkClick("/Downloads")}
+                >
+                  Downloads
+                </NavigationMenu.Trigger>
+              </NavigationMenu.Item>
+
+              <NavigationMenu.Item>
+                <NavigationMenu.Trigger
+                  href="/Contacts"
+                  className={`uppercase outline outline-1 py-1 px-4 hover:outline-gray-300 ${
+                    activeLink === "/Contacts"
+                      ? "outline-gray-300"
+                      : "outline-gray-800"
+                  }`}
+                  onClick={() => handleLinkClick("/Contacts")}
+                >
+                  Contacts
+                </NavigationMenu.Trigger>
+              </NavigationMenu.Item>
+            </NavigationMenu.List>
+          </NavigationMenu.Root> */}
+
           <ul className="flex text-white text-sm font-normal gap-1">
             <li className="uppercase">
               <Link
                 href="/"
-                className={`outline outline-1 py-1 px-4 ${
+                className={`outline outline-1 py-1 px-4 hover:outline-gray-300 ${
                   activeLink === "/" ? "outline-gray-300" : "outline-gray-800"
                 }`}
-                onClick={() => handleLinkClick("/")} // Update active link on click
+                onClick={() => handleLinkClick("/")}
               >
                 home
               </Link>
@@ -65,7 +162,7 @@ const NavMenu = () => {
             <li className="uppercase">
               <Link
                 href="/About"
-                className={`outline outline-1 py-1 px-4 ${
+                className={`outline outline-1 py-1 px-4 hover:outline-gray-300 ${
                   activeLink === "/About"
                     ? "outline-gray-300"
                     : "outline-gray-800"
@@ -78,7 +175,7 @@ const NavMenu = () => {
             <li className="uppercase">
               <Link
                 href="/Services"
-                className={`outline outline-1 py-1 px-4 ${
+                className={`outline outline-1 py-1 px-4 hover:outline-gray-300 ${
                   activeLink === "/Services"
                     ? "outline-gray-300"
                     : "outline-gray-800"
@@ -91,7 +188,7 @@ const NavMenu = () => {
             <li className="uppercase">
               <Link
                 href="/Events"
-                className={`outline outline-1 py-1 px-4 ${
+                className={`outline outline-1 py-1 px-4 hover:outline-gray-300 ${
                   activeLink === "/Events"
                     ? "outline-gray-300"
                     : "outline-gray-800"
@@ -104,7 +201,7 @@ const NavMenu = () => {
             <li className="uppercase">
               <Link
                 href="/Contacts"
-                className={`outline outline-1 py-1 px-4 ${
+                className={`outline outline-1 py-1 px-4 hover:outline-gray-300 ${
                   activeLink === "/Contacts"
                     ? "outline-gray-300"
                     : "outline-gray-800"
@@ -113,6 +210,17 @@ const NavMenu = () => {
               >
                 contacts
               </Link>
+              <Link
+                href="/Downloads"
+                className={`outline outline-1 py-1 px-4 hover:outline-gray-300 ${
+                  activeLink === "/Downloads"
+                    ? "outline-gray-300"
+                    : "outline-gray-800"
+                }`}
+                onClick={() => handleLinkClick("/Downloads")}
+              >
+                downloads
+              </Link>
             </li>
           </ul>
         </div>
@@ -120,5 +228,22 @@ const NavMenu = () => {
     </div>
   );
 };
+
+// const ListItem = React.forwardRef(
+//   ({ className, children, title, ...props }, forwardedRef) => (
+//     <li>
+//       <NavigationMenu.Link asChild>
+//         <a
+//           className={classNames("block p-3 pl-5", className)}
+//           {...props}
+//           ref={forwardedRef}
+//         >
+//           <div className="font-medium">{title}</div>
+//           <p>{children}</p>
+//         </a>
+//       </NavigationMenu.Link>
+//     </li>
+//   )
+// );
 
 export default NavMenu;
