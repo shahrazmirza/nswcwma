8d6c557a4a7bfbf9fe832e383a099b172b661198
shahrazmirza <shahrazahmedmirza@live.com>
2024-07-14 15:47:13 +1000

    Commit

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import ToggleMenu from "./ToggleMenu";
 import NavMenu from "./NavMenu";
+import ToggleMenu from "./ToggleMenu";
 
 const Navbar = () => {
   const [isMobile, setIsMobile] = useState(false);
