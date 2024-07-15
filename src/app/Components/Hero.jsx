9a7e5e0997f91c7e25a724d7291a0e01b00023fb
shahrazmirza <shahrazahmedmirza@live.com>
2024-07-15 14:30:20 +1000

    Commit

--- src/app/Components/Hero.jsx
+++ src/app/Components/Hero.jsx
@@ -14,7 +14,7 @@ function Hero() {
       />
       <div className="absolute flex flex-col items-center justify-center">
         <div className="md:p-10 py-8 w-fit text-center text-white">
-          <div className="flex flex-col border md:p-10 px-12 py-6 gap-5">
+          <div className="flex flex-col border md:p-10 px-10 py-6 gap-5">
             <h1 className="md:text-4xl text-2xl md:font-medium font-semibold md:tracking-widest">
               BE USEFUL TO OTHERS
             </h1>
