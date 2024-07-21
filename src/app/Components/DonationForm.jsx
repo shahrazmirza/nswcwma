f4a172853b1b2a4d14748f210f4bcf4b93cd3b36
Shahraz Mirza <129364236+shahrazmirza@users.noreply.github.com>
2024-07-22 02:17:14 +1000

    Update DonationForm.jsx

--- src/app/Components/DonationForm.jsx
+++ src/app/Components/DonationForm.jsx
@@ -80,8 +80,8 @@ function DonationForm() {
                     key={index}
                     className="btn-sm bg-white hover:bg-white border-none outline outline-1 outline-gray-300 hover:outline-red-500 font-medium text-gray-500"
                     type="radio"
-                    name="options"
-                    aria-label={`$${item.value.toLocaleString()}`}
+                 
+                   aria-label={`$${item.value.toLocaleString()}`}
                     value={item.value}
                     onChange={() => setSelectedAmount(parseFloat(item.value))}
                   />
