ef78a390ee93d3d4f2e76a91c098dabb7ba7f722
shahrazmirza <shahrazahmedmirza@live.com>
2024-07-23 22:11:19 +1000

    Commit

--- src/app/Components/ServicesBanner.jsx
+++ src/app/Components/ServicesBanner.jsx
@@ -1,18 +1,87 @@
+"use client";
+import React, { useEffect, useRef } from "react";
 import { Container } from "@radix-ui/themes";
-import React from "react";
 import { FaPeopleRoof } from "react-icons/fa6";
 import { FaBowlFood } from "react-icons/fa6";
 import { TbDisabled } from "react-icons/tb";
 import { FaCar } from "react-icons/fa";
 import { IoIosPeople } from "react-icons/io";
 import { MdSick } from "react-icons/md";
+import { motion, useAnimation, useInView } from "framer-motion";
 
 function ServicesBanner() {
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+  const ref3 = useRef(null);
+  const ref4 = useRef(null);
+  const ref5 = useRef(null);
+  const ref6 = useRef(null);
+
+  const slide1 = useAnimation();
+  const slide2 = useAnimation();
+  const slide3 = useAnimation();
+  const slide4 = useAnimation();
+  const slide5 = useAnimation();
+  const slide6 = useAnimation();
+
+  const isInView1 = useInView(ref1, { once: true });
+  const isInView2 = useInView(ref2, { once: true });
+  const isInView3 = useInView(ref3, { once: true });
+  const isInView4 = useInView(ref4, { once: true });
+  const isInView5 = useInView(ref5, { once: true });
+  const isInView6 = useInView(ref6, { once: true });
+
+  useEffect(() => {
+    if (isInView1) {
+      slide1.start("visible");
+    }
+  }, [isInView1]);
+
+  useEffect(() => {
+    if (isInView2) {
+      slide2.start("visible");
+    }
+  }, [isInView2]);
+
+  useEffect(() => {
+    if (isInView3) {
+      slide3.start("visible");
+    }
+  }, [isInView3]);
+
+  useEffect(() => {
+    if (isInView4) {
+      slide4.start("visible");
+    }
+  }, [isInView4]);
+
+  useEffect(() => {
+    if (isInView5) {
+      slide5.start("visible");
+    }
+  }, [isInView5]);
+
+  useEffect(() => {
+    if (isInView6) {
+      slide6.start("visible");
+    }
+  }, [isInView6]);
+
   return (
     <div className="bg-gray-700 text-gray-300">
       <Container>
         <div className="flex flex-col md:grid grid-cols-3 gap-12 p-10">
-          <div className="flex justify-start items-center">
+          <motion.div
+            ref={ref1}
+            variants={{
+              hidden: { opacity: 0, y: 75 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            initial="hidden"
+            animate={slide1}
+            transition={{ duration: 0.9, delay: 0 }}
+            className="flex justify-start items-center"
+          >
             <FaPeopleRoof className="text-6xl pr-8" />
 
             <div className="border-l-[1px] border-gray-300 pl-8 flex flex-col gap-5 flex-1">
@@ -24,9 +93,19 @@ function ServicesBanner() {
                 individuals and at-risk travelers.
               </p>
             </div>
-          </div>
+          </motion.div>
 
-          <div className="flex justify-start items-center">
+          <motion.div
+            ref={ref2}
+            variants={{
+              hidden: { opacity: 0, y: 75 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            initial="hidden"
+            animate={slide2}
+            transition={{ duration: 0.9, delay: 0 }}
+            className="flex justify-start items-center"
+          >
             <FaBowlFood className="text-6xl pr-8" />
 
             <div className="border-l-[1px] border-gray-300 pl-8 flex flex-col gap-5 flex-1">
@@ -38,9 +117,19 @@ function ServicesBanner() {
                 and foster social interaction.
               </p>
             </div>
-          </div>
+          </motion.div>
 
-          <div className="flex justify-start items-center">
+          <motion.div
+            ref={ref3}
+            variants={{
+              hidden: { opacity: 0, y: 75 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            initial="hidden"
+            animate={slide3}
+            transition={{ duration: 0.9, delay: 0 }}
+            className="flex justify-start items-center"
+          >
             <TbDisabled className="text-6xl pr-8" />
 
             <div className="border-l-[1px] border-gray-300 pl-8 flex flex-col gap-5 flex-1">
@@ -52,9 +141,19 @@ function ServicesBanner() {
                 paid work opportunities.
               </p>
             </div>
-          </div>
+          </motion.div>
 
-          <div className="flex justify-start items-center">
+          <motion.div
+            ref={ref4}
+            variants={{
+              hidden: { opacity: 0, y: 75 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            initial="hidden"
+            animate={slide4}
+            transition={{ duration: 0.9, delay: 0 }}
+            className="flex justify-start items-center"
+          >
             <FaCar className="text-6xl pr-8" />
 
             <div className="border-l-[1px] border-gray-300 pl-8 flex flex-col gap-5 flex-1">
@@ -66,9 +165,19 @@ function ServicesBanner() {
                 migrants to enhance independence.
               </p>
             </div>
-          </div>
+          </motion.div>
 
-          <div className="flex justify-start items-center">
+          <motion.div
+            ref={ref5}
+            variants={{
+              hidden: { opacity: 0, y: 75 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            initial="hidden"
+            animate={slide5}
+            transition={{ duration: 0.9, delay: 0 }}
+            className="flex justify-start items-center"
+          >
             <IoIosPeople className="text-6xl pr-8" />
 
             <div className="border-l-[1px] border-gray-300 pl-8 flex flex-col gap-5 flex-1">
@@ -80,9 +189,19 @@ function ServicesBanner() {
                 improve their quality of life.
               </p>
             </div>
-          </div>
+          </motion.div>
 
-          <div className="flex justify-start items-center">
+          <motion.div
+            ref={ref6}
+            variants={{
+              hidden: { opacity: 0, y: 75 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            initial="hidden"
+            animate={slide6}
+            transition={{ duration: 0.9, delay: 0 }}
+            className="flex justify-start items-center"
+          >
             <MdSick className="text-6xl pr-8" />
 
             <div className="border-l-[1px] border-gray-300 pl-8 flex flex-col gap-5 flex-1">
@@ -94,7 +213,7 @@ function ServicesBanner() {
                 fighting poverty.
               </p>
             </div>
-          </div>
+          </motion.div>
         </div>
       </Container>
     </div>
--- src/app/Components/Team.jsx
+++ src/app/Components/Team.jsx
@@ -1,15 +1,84 @@
+"use client";
+import React, { useEffect, useRef } from "react";
 import { Container } from "@radix-ui/themes";
 import Image from "next/image";
-import React from "react";
+import { motion, useAnimation, useInView } from "framer-motion";
 
 function Team() {
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+  const ref3 = useRef(null);
+  const ref4 = useRef(null);
+  const ref5 = useRef(null);
+
+  const slide1 = useAnimation();
+  const slide2 = useAnimation();
+  const slide3 = useAnimation();
+  const slide4 = useAnimation();
+  const slide5 = useAnimation();
+
+  const isInView1 = useInView(ref1, { once: true });
+  const isInView2 = useInView(ref2, { once: true });
+  const isInView3 = useInView(ref3, { once: true });
+  const isInView4 = useInView(ref4, { once: true });
+  const isInView5 = useInView(ref5, { once: true });
+
+  useEffect(() => {
+    if (isInView1) {
+      slide1.start("visible");
+    }
+  }, [isInView1]);
+
+  useEffect(() => {
+    if (isInView2) {
+      slide2.start("visible");
+    }
+  }, [isInView2]);
+
+  useEffect(() => {
+    if (isInView3) {
+      slide3.start("visible");
+    }
+  }, [isInView3]);
+
+  useEffect(() => {
+    if (isInView4) {
+      slide4.start("visible");
+    }
+  }, [isInView4]);
+
+  useEffect(() => {
+    if (isInView5) {
+      slide5.start("visible");
+    }
+  }, [isInView5]);
+
   return (
     <Container>
       <div className="md:grid md:grid-cols-5 gap-6 md:py-20 md:p-0 p-5">
-        <div className="flex items-center uppercase text-3xl font-semibold tracking-widest text-gray-800 border-l border-gray-500 pl-5 h-16">
+        <motion.div
+          ref={ref1}
+          variants={{
+            hidden: { opacity: 0, x: -75 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          initial="hidden"
+          animate={slide1}
+          transition={{ duration: 0.9, delay: 0 }}
+          className="flex items-center uppercase text-3xl font-semibold tracking-widest text-gray-800 border-l border-gray-500 pl-5 h-16"
+        >
           our team
-        </div>
-        <div>
+        </motion.div>
+        <motion.div
+          ref={ref2}
+          variants={{
+            hidden: { opacity: 0, y: 75 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          initial="hidden"
+          animate={slide2}
+          transition={{ duration: 0.9, delay: 0 }}
+        >
           <Image
             src="/assets/images/president.jpg"
             alt="President Image"
@@ -26,9 +95,18 @@ function Team() {
               president
             </h2>
           </div>
-        </div>
+        </motion.div>
 
-        <div>
+        <motion.div
+          ref={ref3}
+          variants={{
+            hidden: { opacity: 0, y: 75 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          initial="hidden"
+          animate={slide3}
+          transition={{ duration: 0.9, delay: 0 }}
+        >
           <Image
             src="/assets/images/vice-president.jpg"
             alt="Vice President Image"
@@ -44,9 +122,18 @@ function Team() {
               vice president
             </h2>
           </div>
-        </div>
+        </motion.div>
 
-        <div>
+        <motion.div
+          ref={ref4}
+          variants={{
+            hidden: { opacity: 0, y: 75 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          initial="hidden"
+          animate={slide4}
+          transition={{ duration: 0.9, delay: 0 }}
+        >
           <Image
             src="/assets/images/treasurer.jpg"
             alt="President Image"
@@ -63,9 +150,18 @@ function Team() {
               treasurer
             </h2>
           </div>
-        </div>
+        </motion.div>
 
-        <div>
+        <motion.div
+          ref={ref5}
+          variants={{
+            hidden: { opacity: 0, y: 75 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          initial="hidden"
+          animate={slide5}
+          transition={{ duration: 0.9, delay: 0 }}
+        >
           <Image
             src="/assets/images/secretary.jpg"
             alt="Vice President Image"
@@ -81,7 +177,7 @@ function Team() {
               secretary
             </h2>
           </div>
-        </div>
+        </motion.div>
       </div>
     </Container>
   );
