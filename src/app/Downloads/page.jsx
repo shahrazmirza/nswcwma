dcda10157f58fa16e1aa4c8487bc06a2626c081a
shahrazmirza <shahrazahmedmirza@live.com>
2024-10-05 22:02:14 +1000

    Commit

--- src/app/Downloads/page.jsx
+++ src/app/Downloads/page.jsx
@@ -3,7 +3,7 @@ import Whatsapp from "../Components/Whatsapp";
 import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
 import ScrollToTop from "../Components/ScrollToTop";
-import ReportsAndPolicies from "../Components/ReportsAndPolicies";
+import ReportsAndPolicies from "../Components/";
 
 function Downloads() {
   return (
