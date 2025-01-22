20bdae3e6c5e6a8cdc47df67c331c459024a905a
shahrazmirza <shahrazahmedmirza@live.com>
2025-01-22 18:44:42 +1100

    Vercel Analytics Added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "@radix-ui/themes/styles.css";
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
         <Theme>
           <NextUIProvider>
             {children}
+            <Analytics />
             <ToastContainer />
           </NextUIProvider>
         </Theme>
