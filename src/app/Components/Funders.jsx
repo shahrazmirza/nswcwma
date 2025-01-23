1c39e5d56afafa49ba90c54281398e5a531619ba
shahrazmirza <shahrazahmedmirza@live.com>
2025-01-23 14:25:21 +1100

    Commit

--- src/app/Components/Funders.jsx
+++ src/app/Components/Funders.jsx
@@ -19,21 +19,12 @@ function Funders() {
 
   const ref1 = useRef(null);
   const ref2 = useRef(null);
-  const ref3 = useRef(null);
-  const ref4 = useRef(null);
-  const ref5 = useRef(null);
 
   const slide1 = useAnimation();
   const slide2 = useAnimation();
-  const slide3 = useAnimation();
-  const slide4 = useAnimation();
-  const slide5 = useAnimation();
 
   const isInView1 = useInView(ref1, { once: true });
   const isInView2 = useInView(ref2, { once: true });
-  const isInView3 = useInView(ref3, { once: true });
-  const isInView4 = useInView(ref4, { once: true });
-  const isInView5 = useInView(ref5, { once: true });
 
   useEffect(() => {
     if (isInView1) {
@@ -46,24 +37,6 @@ function Funders() {
       slide2.start("visible");
     }
   }, [isInView2]);
-
-  useEffect(() => {
-    if (isInView3) {
-      slide3.start("visible");
-    }
-  }, [isInView3]);
-
-  useEffect(() => {
-    if (isInView4) {
-      slide4.start("visible");
-    }
-  }, [isInView4]);
-
-  useEffect(() => {
-    if (isInView5) {
-      slide5.start("visible");
-    }
-  }, [isInView5]);
 
   return (
     <Container>
