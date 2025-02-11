b8372a777b3efbc92e3fcf24598f718900d46734
shahrazmirza <shahrazahmedmirza@live.com>
2025-02-11 17:27:59 +1100

    Commit

--- src/app/Components/NavMenu.jsx
+++ src/app/Components/NavMenu.jsx
@@ -70,21 +70,7 @@ const NavMenu = () => {
 
   return (
     <>
-      <div className="flex justify-center items-center bg-black w-screen text-xl text-center p-3 text-yellow-500 font-bold tracking-[.25em]">
-        <Link
-          href="/ZakatCalculator"
-          className="flex justify-center items-center gap-3 animate__animated animate__pulse animate__infinite animate__slow"
-        >
-          <div className="text-3xl text-green-600">
-            <BiSolidDonateHeart />
-          </div>
-          <p className="text-white">Click Here To</p>
-          <p>Pay</p>
-          <p className="text-red-500">ZAKAT</p>
-          <p>Now!</p>
-        </Link>
-      </div>
-      <div className="bg-gray-800 w-screen h-[86px] shadow-d">
+      <div className="bg-gray-800 mt-14 w-screen h-[86px] shadow-d">
         {/* Loading Indicator */}
         {loading && (
           <div className="flex justify-center items-center h-screen">
@@ -94,178 +80,195 @@ const NavMenu = () => {
         )}
 
         {!loading && (
-          <Container
-            className={`fixed top-14 left-0 right-0 transition-transform transform z-50 ${
+          <div
+            className={`fixed top-0 left-0 right-0 transition-transform transform z-50 ${
               isScrolled ? "-translate-y-full" : "translate-y-0"
             }`}
           >
-            <div className="navbar text-white">
-              <div className="flex-1 px-2 lg:flex-none gap-5">
-                <Link href="/">
-                  <Image
-                    src="/assets/images/logo.png"
-                    alt="NSWCWMA Logo"
-                    width="60"
-                    height="60"
-                  />
-                </Link>
+            <div className="flex justify-center items-center bg-black text-xl text-center p-3 text-yellow-500 font-bold tracking-[.25em]">
+              <Link
+                href="/ZakatCalculator"
+                className="flex justify-center items-center gap-3 animate__animated animate__pulse animate__infinite animate__slow"
+              >
+                <div className="text-3xl text-green-600">
+                  <BiSolidDonateHeart />
+                </div>
+                <p className="text-white">Click Here To</p>
+                <p>Pay</p>
+                <p className="text-red-500">ZAKAT</p>
+                <p>Now!</p>
+              </Link>
+            </div>
+            <Container>
+              <div className="navbar text-white">
+                <div className="flex-1 px-2 lg:flex-none gap-5">
+                  <Link href="/">
+                    <Image
+                      src="/assets/images/logo.png"
+                      alt="NSWCWMA Logo"
+                      width="60"
+                      height="60"
+                    />
+                  </Link>
 
-                <Link href="/">
-                  <h1 className="text-lg font-basic flex flex-col justify-center items-center">
-                    NSW Central West <b></b> Muslims Association
-                  </h1>
-                </Link>
-              </div>
-              <div className="flex flex-1 justify-end px-2 uppercase">
-                <div className="flex items-stretch text-sm font-normal gap-1">
-                  <div className="justify-center items-center flex gap-1">
-                    <Link
-                      href="/"
-                      className={`outline outline-1 px-3 hover:outline-gray-300 ${
-                        activeLink === "/"
-                          ? "outline-gray-300"
-                          : "outline-gray-800"
-                      }`}
-                      onClick={() => setActiveLink("/")}
-                      data-translate
-                      data-original-text="home"
-                    >
-                      home
-                    </Link>
-                  </div>
-                  <div className="dropdown dropdown-hover">
-                    <div
-                      tabIndex={0}
-                      role="button"
-                      className={`outline outline-1 px-3 hover:outline-gray-300 ${
-                        activeLink === "/About" || activeLink === "/Mosque"
-                          ? "outline-gray-300"
-                          : "outline-gray-800"
-                      }`}
-                      onClick={() => setActiveLink("/About")}
-                      data-translate
-                      data-original-text="about"
-                    >
-                      about
+                  <Link href="/">
+                    <h1 className="text-lg font-basic flex flex-col justify-center items-center">
+                      NSW Central West <b></b> Muslims Association
+                    </h1>
+                  </Link>
+                </div>
+                <div className="flex flex-1 justify-end px-2 uppercase">
+                  <div className="flex items-stretch text-sm font-normal gap-1">
+                    <div className="justify-center items-center flex gap-1">
+                      <Link
+                        href="/"
+                        className={`outline outline-1 px-3 hover:outline-gray-300 ${
+                          activeLink === "/"
+                            ? "outline-gray-300"
+                            : "outline-gray-800"
+                        }`}
+                        onClick={() => setActiveLink("/")}
+                        data-translate
+                        data-original-text="home"
+                      >
+                        home
+                      </Link>
+                    </div>
+                    <div className="dropdown dropdown-hover">
+                      <div
+                        tabIndex={0}
+                        role="button"
+                        className={`outline outline-1 px-3 hover:outline-gray-300 ${
+                          activeLink === "/About" || activeLink === "/Mosque"
+                            ? "outline-gray-300"
+                            : "outline-gray-800"
+                        }`}
+                        onClick={() => setActiveLink("/About")}
+                        data-translate
+                        data-original-text="about"
+                      >
+                        about
+                      </div>
+
+                      <ul
+                        tabIndex={0}
+                        className="dropdown-content menu bg-gray-800 rounded-md z-[1] mt-1 w-36 shadow p-0 -ml-1"
+                      >
+                        <li>
+                          <Link
+                            href="/About"
+                            data-translate
+                            data-original-text="about"
+                          >
+                            Who We Are
+                          </Link>
+                        </li>
+                        <li>
+                          <Link
+                            href="/Mosque"
+                            data-translate
+                            data-original-text="mosque"
+                          >
+                            mosque
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="justify-center items-center flex gap-1">
+                      <Link
+                        href="/Services"
+                        className={`outline outline-1 px-3 hover:outline-gray-300 ${
+                          activeLink === "/Services"
+                            ? "outline-gray-300"
+                            : "outline-gray-800"
+                        }`}
+                        onClick={() => setActiveLink("/Services")}
+                        data-translate
+                        data-original-text="services"
+                      >
+                        services
+                      </Link>
+                    </div>
+                    <div className="justify-center items-center flex gap-1">
+                      <Link
+                        href="/Events"
+                        className={`outline outline-1 px-3 hover:outline-gray-300 ${
+                          activeLink === "/Events"
+                            ? "outline-gray-300"
+                            : "outline-gray-800"
+                        }`}
+                        onClick={() => setActiveLink("/Events")}
+                        data-translate
+                        data-original-text="events"
+                      >
+                        events
+                      </Link>
                     </div>
 
-                    <ul
-                      tabIndex={0}
-                      className="dropdown-content menu bg-gray-800 rounded-md z-[1] mt-1 w-36 shadow p-0 -ml-1"
-                    >
-                      <li>
-                        <Link
-                          href="/About"
-                          data-translate
-                          data-original-text="about"
-                        >
-                          Who We Are
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          href="/Mosque"
-                          data-translate
-                          data-original-text="mosque"
-                        >
-                          mosque
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="justify-center items-center flex gap-1">
-                    <Link
-                      href="/Services"
-                      className={`outline outline-1 px-3 hover:outline-gray-300 ${
-                        activeLink === "/Services"
-                          ? "outline-gray-300"
-                          : "outline-gray-800"
-                      }`}
-                      onClick={() => setActiveLink("/Services")}
-                      data-translate
-                      data-original-text="services"
-                    >
-                      services
-                    </Link>
-                  </div>
-                  <div className="justify-center items-center flex gap-1">
-                    <Link
-                      href="/Events"
-                      className={`outline outline-1 px-3 hover:outline-gray-300 ${
-                        activeLink === "/Events"
-                          ? "outline-gray-300"
-                          : "outline-gray-800"
-                      }`}
-                      onClick={() => setActiveLink("/Events")}
-                      data-translate
-                      data-original-text="events"
-                    >
-                      events
-                    </Link>
-                  </div>
-
-                  <div className="dropdown dropdown-hover">
-                    <div
-                      tabIndex={0}
-                      role="button"
-                      className={`outline outline-1 px-3 hover:outline-gray-300 ${
-                        activeLink === "/Careers" || activeLink === "/Downloads"
-                          ? "outline-gray-300"
-                          : "outline-gray-800"
-                      }`}
-                      onClick={() => setActiveLink("/Resources")}
-                      data-translate
-                      data-original-text="about"
-                    >
-                      resources
+                    <div className="dropdown dropdown-hover">
+                      <div
+                        tabIndex={0}
+                        role="button"
+                        className={`outline outline-1 px-3 hover:outline-gray-300 ${
+                          activeLink === "/Careers" ||
+                          activeLink === "/Downloads"
+                            ? "outline-gray-300"
+                            : "outline-gray-800"
+                        }`}
+                        onClick={() => setActiveLink("/Resources")}
+                        data-translate
+                        data-original-text="about"
+                      >
+                        resources
+                      </div>
+                      <ul
+                        tabIndex={0}
+                        className="dropdown-content menu bg-gray-800 rounded-md z-[1] mt-1 w-36 shadow p-0 -ml-1"
+                      >
+                        <li>
+                          <Link
+                            href="/Careers"
+                            data-translate
+                            data-original-text="careers"
+                          >
+                            careers
+                          </Link>
+                        </li>
+                        <li>
+                          <Link
+                            href="/Downloads"
+                            data-translate
+                            data-original-text="downloads"
+                          >
+                            downloads
+                          </Link>
+                        </li>
+                      </ul>
                     </div>
-                    <ul
-                      tabIndex={0}
-                      className="dropdown-content menu bg-gray-800 rounded-md z-[1] mt-1 w-36 shadow p-0 -ml-1"
-                    >
-                      <li>
-                        <Link
-                          href="/Careers"
-                          data-translate
-                          data-original-text="careers"
-                        >
-                          careers
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          href="/Downloads"
-                          data-translate
-                          data-original-text="downloads"
-                        >
-                          downloads
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
 
-                  <div className="justify-center items-center flex gap-1">
-                    <Link
-                      href="/Contacts"
-                      className={`outline outline-1 px-3 hover:outline-gray-300 ${
-                        activeLink === "/Contacts"
-                          ? "outline-gray-300"
-                          : "outline-gray-800"
-                      }`}
-                      onClick={() => setActiveLink("/Contacts")}
-                      data-translate
-                      data-original-text="contacts"
-                    >
-                      contacts
-                    </Link>
-                  </div>
-                  <div className="justify-center items-center flex gap-1">
-                    <Languages />
+                    <div className="justify-center items-center flex gap-1">
+                      <Link
+                        href="/Contacts"
+                        className={`outline outline-1 px-3 hover:outline-gray-300 ${
+                          activeLink === "/Contacts"
+                            ? "outline-gray-300"
+                            : "outline-gray-800"
+                        }`}
+                        onClick={() => setActiveLink("/Contacts")}
+                        data-translate
+                        data-original-text="contacts"
+                      >
+                        contacts
+                      </Link>
+                    </div>
+                    <div className="justify-center items-center flex gap-1">
+                      <Languages />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </Container>
+            </Container>
+          </div>
         )}
       </div>
     </>
